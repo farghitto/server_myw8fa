1ec62da90c2a53c5f65fab203d242f1bd60aacbd
Marco Prosperi <savemrc@gmail.com>
2023-10-06 13:36:49 +0200

    logout

--- server_myw8fa/urls.py
+++ server_myw8fa/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from utente.apiview import CustomAuthToken
+from utente.apiview import CustomAuthToken, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +28,5 @@ urlpatterns = [
 
 
     path('autenticazione', CustomAuthToken.as_view(), name='token_obtain'),
-
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
--- utente/apiview.py
+++ utente/apiview.py
@@ -1,12 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
 from django.contrib.auth import authenticate
 
 
-
 class CustomAuthToken(APIView):
-    
+
     def post(self, request, format=None):
         username = request.data.get('username')
         password = request.data.get('password')
@@ -14,6 +17,17 @@ class CustomAuthToken(APIView):
 
         if user:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, 'user_id' : user.id})
+            return Response({'token': token.key, 'user_id': user.id})
         else:
             return Response({'error': 'Credenziali non valide'}, status=401)
+
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # Invalida o elimina il token dell'utente
+        request.auth.delete()  # Elimina il token di autenticazione personalizzato
+
+        return Response(status.HTTP_200_OK)

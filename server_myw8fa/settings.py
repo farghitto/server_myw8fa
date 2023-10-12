094907c663d7d7f4f258e1642d4479ddd9ad35b8
pve <you@example.com>
2023-10-12 08:38:14 +0000

    aggiorna

--- server_myw8fa/settings.py
+++ server_myw8fa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=wvn&1*)10=ifie-ta*uagm3obnv2@@rf9n%6jt542+ptmo9yw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

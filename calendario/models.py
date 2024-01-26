7ab44bf7e51bbf726942c3039e67f31defa87ec4
farghitto <farghito@hotmail.com>
2024-01-26 16:18:19 +0100

    calendario

--- calendario/models.py
+++ calendario/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Appuntamento(models.Model):
     descrizione = models.TextField(blank=True, null=True)
     inizio = models.DateTimeField()
     fine = models.DateTimeField()
+    effettuato = models.BooleanField( default = "False")
 
     def __str__(self):
         return self.titolo
